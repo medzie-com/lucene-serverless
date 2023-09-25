ac60ef5c6894cbd086195c22aba736cf14bb06cd
npenin <nicolas.penin@medzie.com>
2023-09-25 15:26:16 +0200

    improve index handler

--- src/main/java/dev/arseny/handler/IndexHandler.java
+++ src/main/java/dev/arseny/handler/IndexHandler.java
@@ -10,7 +10,9 @@ import dev.arseny.service.IndexWriterService;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.Term;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
@@ -20,6 +22,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Stream;
+
+class Pair<T, U> {
+    private T left;
+    private U right;
+
+    public Pair(T left, U right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    public T getLeft() {
+        return this.left;
+    }
+
+    public U getRight() {
+        return this.right;
+    }
+}
 
 @Named("index")
 public class IndexHandler implements RequestHandler<SQSEvent, Integer> {
@@ -49,18 +70,20 @@ public class IndexHandler implements RequestHandler<SQSEvent, Integer> {
                 writerMap.put(request.getIndexName(), writer);
             }
 
-            List<Document> documents = new ArrayList<>();
+            List<Pair<Term, Document>> documents = new ArrayList<>();
 
             for (Map<String, Object> requestDocument : request.getDocuments()) {
                 Document document = new Document();
                 for (Map.Entry<String, Object> entry : requestDocument.entrySet()) {
                     document.add(new TextField(entry.getKey(), entry.getValue().toString(), Field.Store.YES));
                 }
-                documents.add(document);
+
+                documents.add(new Pair<Term, Document>(new Term("id", (String) requestDocument.get("id")), document));
             }
 
             try {
-                writer.addDocuments(documents);
+                writer.deleteDocuments((Term[]) documents.stream().map(v -> v.getLeft()).toArray());
+                writer.addDocuments(documents.stream().map(v -> v.getRight()).toList());
             } catch (IOException e) {
                 LOG.error(e);
             }
